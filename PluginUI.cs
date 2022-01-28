fe55dd4dbff435809830aad29be5a83d41d2bc4b
Spencer-star <69193850+Spencer-star@users.noreply.github.com>
2022-01-28 11:25:25 +0800

    Update PluginUI.cs

--- PluginUI.cs
+++ PluginUI.cs
@@ -1,4 +1,4 @@
-﻿using ImGuiNET;
+using ImGuiNET;
 using System.Numerics;
 using Dalamud.Game.ClientState;
 
@@ -80,63 +80,63 @@ namespace DeepDungeonDex
             ImGui.SetNextWindowSizeConstraints(new Vector2(250, 0), new Vector2(9001, 9001));
             ImGui.SetNextWindowBgAlpha(config.Opacity);
             ImGui.Begin("cool strati window", flags);
-            ImGui.Text("Name:\n"+TargetData.Name);
+            ImGui.Text("怪物名称:\n"+TargetData.Name);
             ImGui.NewLine();
             ImGui.Columns(3, null, false);
-            ImGui.Text("Aggro Type:\n");
+            ImGui.Text("攻击类型:\n");
             ImGui.Text(mobData.Aggro.ToString());
             ImGui.NextColumn();
-            ImGui.Text("Threat:\n");
+            ImGui.Text("威胁程度:\n");
             switch (mobData.Threat)
             {
                 case DataHandler.MobData.ThreatLevel.Easy:
                     ImGui.PushStyleColor(ImGuiCol.Text, 0xFF00FF00);
-                    ImGui.Text("Easy");
+                    ImGui.Text("简单");
                     ImGui.PopStyleColor();
                     break;
                 case DataHandler.MobData.ThreatLevel.Caution:
                     ImGui.PushStyleColor(ImGuiCol.Text, 0xFF00FFFF);
-                    ImGui.Text("Caution");
+                    ImGui.Text("需小心");
                     ImGui.PopStyleColor();
                     break;
                 case DataHandler.MobData.ThreatLevel.Dangerous:
                     ImGui.PushStyleColor(ImGuiCol.Text, 0xFF0000FF);
-                    ImGui.Text("Dangerous");
+                    ImGui.Text("危险");
                     ImGui.PopStyleColor();
                     break;
                 case DataHandler.MobData.ThreatLevel.Vicious:
                     ImGui.PushStyleColor(ImGuiCol.Text, 0xFFFF00FF);
-                    ImGui.Text("Vicious");
+                    ImGui.Text("非常危险");
                     ImGui.PopStyleColor();
                     break;
                 default:
-                    ImGui.Text("Undefined");
+                    ImGui.Text("未定义难度");
                     break;
             }
             ImGui.NextColumn();
             if (!config.HideBasedOnJob || cjstun[cjid])
             {
-                PrintSingleVuln(mobData.Vuln.CanStun, "Stun");    
+                PrintSingleVuln(mobData.Vuln.CanStun, "可以击晕");    
             }
             if (!config.HideBasedOnJob || cjsleep[cjid])
             {
-                PrintSingleVuln(mobData.Vuln.CanSleep, "Sleep");    
+                PrintSingleVuln(mobData.Vuln.CanSleep, "可以睡眠");    
             }
             if (!config.HideBasedOnJob || cjbind[cjid])
             {
-                PrintSingleVuln(mobData.Vuln.CanBind, "Bind");
+                PrintSingleVuln(mobData.Vuln.CanBind, "可以定身");
             }
             if (!config.HideBasedOnJob || cjheavy[cjid])
             {
-                PrintSingleVuln(mobData.Vuln.CanHeavy, "Heavy");   
+                PrintSingleVuln(mobData.Vuln.CanHeavy, "可以减速（沉重）");   
             }
             if (!config.HideBasedOnJob || cjslow[cjid])
             {
-                PrintSingleVuln(mobData.Vuln.CanSlow, "Slow");   
+                PrintSingleVuln(mobData.Vuln.CanSlow, "可以减速（缓行）");   
             }
             if (!(TargetData.NameID >= 7262 && TargetData.NameID <= 7610))
             {
-                PrintSingleVuln(mobData.Vuln.IsUndead, "Undead");
+                PrintSingleVuln(mobData.Vuln.IsUndead, "不死族");
             }
             ImGui.NextColumn();
             ImGui.Columns(1);
